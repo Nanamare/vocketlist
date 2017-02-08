b18d0e3128a76364645b70ca0e1601d3b35c67cd
seungtaek.lim <seungtaek.lim@navercorp.com>
2017-02-08 22:06:41 +0900

    배포키 생성 및 설정

---
+++ app/src/main/java/com/vocketlist/android/HashtagsSpanRenderer.java
@@ -0,0 +1,25 @@
+package com.vocketlist.android;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jmpergar.awesometext.AwesomeTextHandler;
+import com.vongtome.android.common.util.ScreenUtils;
+
+public class HashtagsSpanRenderer implements AwesomeTextHandler.ViewSpanRenderer {
+    private final static int textSizeInDips = 18;
+    private final static int backgroundResource = R.drawable.common_hashtags_background;
+    private final static int textColorResource = android.R.color.white;
+
+    @Override
+    public View getView(String text, Context context) {
+        TextView view = new TextView(context);
+        view.setText(text.substring(1));
+        view.setTextSize(ScreenUtils.dipsToPixels(context, textSizeInDips));
+        view.setBackgroundResource(backgroundResource);
+        int textColor = context.getResources().getColor(textColorResource);
+        view.setTextColor(textColor);
+        return view;
+    }
+}
