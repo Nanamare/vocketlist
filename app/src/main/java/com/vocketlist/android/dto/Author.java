d5e9192b428ad2a346b8d21b038e3ef9853ec571
threeword-mac <dev@threeword.com>
2017-02-14 17:57:26 +0900

    커뮤니티 더미 리스트 작성

---
+++ app/src/main/java/com/vocketlist/android/dto/Author.java
@@ -0,0 +1,14 @@
+package com.vocketlist.android.dto;
+
+import java.io.Serializable;
+
+
+/**
+ * 데이터 : 작성자(유저) : 더미
+ *
+ * @author Jungho Song (dev@threeword.com)
+ * @since 2017. 2. 14.
+ */
+public class Author implements Serializable {
+    private String profilePhoto;
+}
