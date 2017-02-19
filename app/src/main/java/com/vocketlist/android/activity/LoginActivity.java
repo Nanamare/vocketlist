e0d98178c95e401442289f829a2f22504a1c643b
nanamare\kinamare <dan.shin@liveo.me>
2017-02-20 02:47:17 +0900

    [기능 구현] 페이스북 유저 정보 받기

--- app/src/main/java/com/vocketlist/android/activity/LoginActivity.java
+++ app/src/main/java/com/vocketlist/android/activity/LoginActivity.java
@@ -2,75 +2,111 @@ package com.vocketlist.android.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.vocketlist.android.R;
 import com.vocketlist.android.roboguice.log.Ln;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.Observable;
 
 /**
  * Created by SeungTaek.Lim on 2017. 2. 2..
  */
 
 public class LoginActivity extends BaseActivity {
-    @BindView(R.id.facebook_login_button) protected LoginButton facebookLoginButton;
+	@BindView(R.id.facebook_login_button)
+	protected LoginButton facebookLoginButton;
 
-    protected CallbackManager callbackManager;
+	protected CallbackManager callbackManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_login);
-        ButterKnife.bind(this);
+		setContentView(R.layout.activity_login);
+		ButterKnife.bind(this);
 
-        this.callbackManager = CallbackManager.Factory.create();
-    }
+		this.callbackManager = CallbackManager.Factory.create();
+	}
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
 
-    @OnClick
-    protected void onClickFacebookLoginButton() {
-        AccessToken token = AccessToken.getCurrentAccessToken();
+	@OnClick
+	protected void onClickFacebookLoginButton() {
+		AccessToken token = AccessToken.getCurrentAccessToken();
 
-        if (token != null) {
-            // login이 되어있는 경우.
-            return;
-        }
+		if (token != null) {
+			// login이 되어있는 경우.
+			return;
+		}
 
-        this.facebookLoginButton.setReadPermissions("email");
-        this.facebookLoginButton.registerCallback(this.callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Ln.d("LoginManager.onSuccess");
-            }
+		this.facebookLoginButton.setReadPermissions("email");
+		this.facebookLoginButton.registerCallback(this.callbackManager, new FacebookCallback<LoginResult>() {
+			@Override
+			public void onSuccess(final LoginResult loginResult) {
+				Ln.d("LoginManager.onSuccess");
+				AccessToken accessToken = loginResult.getAccessToken();
+				DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String expiredAt = sdf.format(accessToken.getExpires().getTime());
 
-            @Override
-            public void onCancel() {
-                Ln.d("LoginManager.onCancel");
-            }
+				GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+					@Override
+					public void onCompleted(JSONObject object, GraphResponse response) {
+						try {
+							//처리 로직 필요
+							Log.d("facebook object",object.toString());
+							String emial = object.getString("email");
+							String name = object.getString("name");
+							String gender = object.getString("gender");
+							String id = object.getString("id");
 
-            @Override
-            public void onError(FacebookException exception) {
-                Ln.d(exception, "LoginManager.onError");
-            }
-        });
-    }
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+				Bundle parameter = new Bundle();
+				parameter.putString("filed","id,name,email,gender");
+				request.setParameters(parameter);
+				request.executeAsync();
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        this.callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
+			}
+
+			@Override
+			public void onCancel() {
+				Ln.d("LoginManager.onCancel");
+			}
+
+			@Override
+			public void onError(FacebookException exception) {
+				Ln.d(exception, "LoginManager.onError");
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		this.callbackManager.onActivityResult(requestCode, resultCode, data);
+	}
 }
