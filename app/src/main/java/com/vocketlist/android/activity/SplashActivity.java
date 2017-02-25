13f18a62b489b875920f12669e756ba95f1e5a95
seungtaek.lim <devlsit81@gmail.com>
2017-02-25 16:31:58 +0900

    동영상

--- app/src/main/java/com/vocketlist/android/activity/SplashActivity.java
+++ app/src/main/java/com/vocketlist/android/activity/SplashActivity.java
@@ -1,38 +1,85 @@
 package com.vocketlist.android.activity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.VideoView;
 
-import com.vocketlist.android.activity.BaseActivity;
 import com.vocketlist.android.R;
-import com.vocketlist.android.activity.MainActivity;
-import com.vocketlist.android.net.ServiceManager;
+import com.vocketlist.android.roboguice.log.Ln;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by lsit on 2017. 1. 30..
  */
 
 public class SplashActivity extends BaseActivity {
+    @BindView(R.id.intro_video) protected VideoView mIntroVideoView;
+    @BindView(R.id.skin) protected View mSkinView;
+
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        mHandler = new Handler();
+        playLogo();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
 
-        new Handler().postDelayed(new Runnable() {
+    private void playLogo() {
+        final String uriPath = "android.resource://" + getPackageName() + "/" + R.raw.spalsh_logo;
+
+        mIntroVideoView.setVideoURI(Uri.parse(uriPath));
+        mIntroVideoView.requestFocus();
+        mIntroVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
-            public void run() {
-                startMainActivity();
-            }
-        }, 1000);
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Ln.e("play error");
+                mIntroVideoView.setVisibility(View.GONE);
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startMainActivity();
+                    }
+                }, 1000);
 
+                return false;
+            }
+        });
+
+        mIntroVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+               startMainActivity();
+            }
+        });
+        mIntroVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSkinView.setVisibility(View.GONE);
+                    }
+                }, 700);
+
+            }
+        });
     }
 
     private void startMainActivity() {
