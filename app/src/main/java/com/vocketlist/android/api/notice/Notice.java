900902dd5893cf26159377bd7b9a15476a3a000a
threeword-mac <dev@threeword.com>
2017-04-08 13:52:21 +0900

    공지사항 데이모델터 추가

---
+++ app/src/main/java/com/vocketlist/android/api/notice/Notice.java
@@ -0,0 +1,17 @@
+package com.vocketlist.android.api.notice;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * 공지사항
+ *
+ * @author Jungho Song (dev@threeword.com)
+ * @since 2017. 4. 8.
+ */
+public class Notice {
+    @SerializedName("id") public Integer mId;
+    @SerializedName("title") public String mTitle;
+    @SerializedName("content") public String mContent;
+    @SerializedName("link") public String mLink;
+    @SerializedName("timestamp") public String timestamp;
+}
