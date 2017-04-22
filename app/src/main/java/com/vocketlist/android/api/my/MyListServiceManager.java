036b1c4bf13a36b87b0835196612708de2e9407e
seungtaek.lim <devlsit81@gmail.com>
2017-04-22 14:35:50 +0900

    MYList api

--- app/src/main/java/com/vocketlist/android/api/my/MyListServiceManager.java
+++ app/src/main/java/com/vocketlist/android/api/my/MyListServiceManager.java
@@ -1,8 +1,47 @@
 package com.vocketlist.android.api.my;
 
+import com.vocketlist.android.api.BaseServiceErrorChecker;
+import com.vocketlist.android.api.ServiceDefine;
+import com.vocketlist.android.dto.BaseResponse;
+import com.vocketlist.android.network.executor.Priority;
+import com.vocketlist.android.network.service.ServiceErrorChecker;
+import com.vocketlist.android.network.service.ServiceHelper;
+
+import java.util.List;
+
+import retrofit2.Response;
+import rx.Observable;
+
 /**
  * Created by SeungTaek.Lim on 2017. 4. 22..
  */
 
 public final class MyListServiceManager {
+    private static final int DEFAULT_PAGE_SIZE = 20;
+
+    private static final MyListService SERVICE = ServiceDefine.retrofit.create(MyListService.class);
+
+    public static Observable<Response<BaseResponse<MyListContent>>> write(String content, boolean isDone) {
+        return SERVICE.write(content, isDone)
+                .subscribeOn(ServiceHelper.getPriorityScheduler(Priority.MEDIUM))
+                .lift(new ServiceErrorChecker<>(new BaseServiceErrorChecker<MyListContent>()));
+    }
+
+    public static Observable<Response<BaseResponse<MyListContent>>> modify(int id, String content, boolean isDone) {
+        return SERVICE.modify(id, content, isDone)
+                .subscribeOn(ServiceHelper.getPriorityScheduler(Priority.MEDIUM))
+                .lift(new ServiceErrorChecker<>(new BaseServiceErrorChecker<MyListContent>()));
+    }
+
+    public static Observable<Response<BaseResponse<List<MyListContent>>>> get(int page) {
+        return SERVICE.get(page, DEFAULT_PAGE_SIZE)
+                .subscribeOn(ServiceHelper.getPriorityScheduler(Priority.MEDIUM))
+                .lift(new ServiceErrorChecker<>(new BaseServiceErrorChecker<List<MyListContent>>()));
+    }
+
+    public static Observable<Response<BaseResponse<Void>>> delete(int id) {
+        return SERVICE.delete(id)
+                .subscribeOn(ServiceHelper.getPriorityScheduler(Priority.MEDIUM))
+                .lift(new ServiceErrorChecker<>(new BaseServiceErrorChecker<Void>()));
+    }
 }
