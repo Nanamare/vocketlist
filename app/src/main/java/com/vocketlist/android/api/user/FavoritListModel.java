d590615ab770cf6b2f1187d318ccf5f1bbd1e6a5
seungtaek.lim <devlsit81@gmail.com>
2017-04-22 19:46:14 +0900

    프로필 > 관심설정 변경 처리

--- app/src/main/java/com/vocketlist/android/api/user/FavoritListModel.java
+++ app/src/main/java/com/vocketlist/android/api/user/FavoritListModel.java
@@ -1,9 +1,13 @@
 package com.vocketlist.android.api.user;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by SeungTaek.Lim on 2017. 4. 22..
  */
 
 public class FavoritListModel {
-    Favorit a;
+    @SerializedName("favorite") public List<String> mFavoriteList;
 }
