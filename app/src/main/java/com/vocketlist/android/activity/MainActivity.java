075e584e1b743558c2b6a1588ae6ae88cf5ceabc
threeword-mac <dev@threeword.com>
2017-05-13 23:09:36 +0900

    좋아요

--- app/src/main/java/com/vocketlist/android/activity/MainActivity.java
+++ app/src/main/java/com/vocketlist/android/activity/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
